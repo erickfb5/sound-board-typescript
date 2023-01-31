59d27360e0a62b3b618f449959cdfaa6b9da03c4
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-01-31 00:20:25 -0300

    second version

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,36 @@
 import { useState, useEffect } from "react";
 
-import { sounds } from "./sounds";
+import { mp3, sounds } from "./sounds";
 import "./App.css";
 
 const App = () => {
-  const [selectedSound, setSelectedSound] = useState<string>('');
-  const audioElements = sounds.map(({ id, src }) => (
-    <audio id={id} key={id} src={src} />
-  ));
+  const [selectedSound, setSelectedSound] = useState<string>("");
 
   useEffect(() => {
     if (selectedSound) {
-      sounds.forEach(({ id }) => {
-        const song = document.getElementById(id) as HTMLAudioElement;
+      sounds.forEach((sound: string) => {
+        const song = document.getElementById(sound) as HTMLAudioElement;
         if (song) {
           song.pause();
           song.currentTime = 0;
         }
       });
 
-      const selectedSong = document.getElementById(
-        selectedSound
-      ) as HTMLAudioElement;
-
+      const selectedSong = document.getElementById(selectedSound) as HTMLAudioElement;
       if (selectedSong) selectedSong.play();
     }
   }, [selectedSound]);
 
-  const buttons = sounds.map(({ id, src }) => {
-    return (
-      <button className="btn" key={src} onClick={() => setSelectedSound(id)}>
-        {id}
+  const soundButtons = sounds.map((sound: string) => (
+    <>
+      <audio id={sound} key={sound} src={mp3(sound)} />
+      <button className="btn"key={sound}onClick={() => setSelectedSound(sound)}>
+        {sound}
       </button>
-    );
-  });
+    </>
+  ));
 
-  return (
-    <div>
-      {audioElements}
-      <div id="buttons">{buttons}</div>
-    </div>
-  );
+  return <div>{soundButtons}</div>;
 };
 
 export default App;
--- src/sounds/index.ts
+++ src/sounds/index.ts
@@ -1,13 +1,11 @@
-export interface Sound {
-  id: string;
-  src: string;
-}
+export const mp3 = (sound: string) => require(`./${sound}.mp3`)
 
-export const sounds: Sound[] = [
-  { id: "applause", src: require("./applause.mp3") },
-  { id: "boo", src: require("./boo.mp3") },
-  { id: "gasp", src: require("./gasp.mp3") },
-  { id: "tada", src: require("./tada.mp3") },
-  { id: "victory", src: require("./victory.mp3") },
-  { id: "wrong", src: require("./wrong.mp3") },
-];
+export const sounds: string[] = [
+    "applause",
+    "boo",
+    "gasp",
+    "tada",
+    "victory",
+    "wrong",
+  ];
+  
